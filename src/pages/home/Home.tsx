d9cdc5d354a20bdf434c6b8137fe54a64571b4e4
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2025-02-24 23:41:25 +0300

    react recharts completed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
+import { PieChart } from "recharts";
+import BarChartBox from "../../components/barChartBox/BarChartBox";
 import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/TopBox";
 import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
   chartBoxConversion,
   chartBoxProduct,
   chartBoxRevenue,
   chartBoxUser,
 } from "../../data";
 import "./home.scss";
+import PieChartBox from "../../components/pieChartBox/PieChartBox";
+import BigChartBox from "../../components/bigCahrtBox/BigChartBox";
 
 const Home = () => {
   return (
@@ -20,16 +26,24 @@ const Home = () => {
       <div className='box box3'>
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className='box box4'>Box4</div>
+      <div className='box box4'>
+        <PieChartBox />
+      </div>
       <div className='box box5'>
         <ChartBox {...chartBoxConversion} />
       </div>
       <div className='box box6'>
         <ChartBox {...chartBoxRevenue} />
       </div>
-      <div className='box box7'>Box7</div>
-      <div className='box box8'>Box8</div>
-      <div className='box box9'>Box9</div>
+      <div className='box box7'>
+        <BigChartBox />
+      </div>
+      <div className='box box8'>
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className='box box9'>
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 };
